cb1943312688c72105a8d86daf122631ab2e720d
sajjad <faniam321@gmail.com>
2023-02-10 15:38:39 +0330

    change name of fields

--- communication/models.py
+++ communication/models.py
@@ -4,11 +4,11 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Message(models.Model):
-    user = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, verbose_name=_('User'), related_name='message_user'
-    )
     from_user = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, verbose_name=_('From user'), related_name='message_from_user'
+    )
+    to_user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, verbose_name=_('To user'), related_name='message_to_user'
     )
     message = models.TextField(verbose_name=_('Message'))
     created_at = models.DateTimeField(auto_now_add=True)
