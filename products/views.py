ace22a34e990a24e26c1d0f44b4c077e5ba57953
sajjad <faniam321@gmail.com>
2023-02-10 22:58:41 +0330

    add filter

--- products/views.py
+++ products/views.py
@@ -69,6 +69,9 @@ class CreateProduct(CreateAPIView):
 class ProductList(ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    filterset_fields = [
+        'warranty', 'category', 'title', 'description', 'color', 'material'
+    ]
 
 
 class UpdateProduct(RetrieveUpdateDestroyAPIView):
