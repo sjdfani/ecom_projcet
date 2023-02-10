896b45ee75cca10977f381152fdbb5ef20b64a12
sajjad <faniam321@gmail.com>
2023-02-10 12:00:15 +0330

    create custom user model

--- users/manager.py
+++ users/manager.py
@@ -9,9 +9,6 @@ class CustomUserManager(BaseUserManager):
     """
 
     def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given email and password.
-        """
         if not email:
             raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
@@ -21,9 +18,6 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
--- users/models.py
+++ users/models.py
@@ -5,9 +5,37 @@ from django.utils.translation import gettext_lazy as _
 from .manager import CustomUserManager
 
 
+class Gender(models.TextChoices):
+    MALE = ('male', 'Male')
+    FEMALE = ('female', 'Female')
+    NONE = ('none', 'None')
+
+
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    fullname = models.CharField(
+        max_length=50, verbose_name=_('Fullname'), null=True, blank=True)
+    home = models.CharField(
+        max_length=11, verbose_name=_('Home'), null=True, blank=True)
+    phone = models.CharField(
+        max_length=11, verbose_name=_('Phone'), null=True, blank=True
+    )
+    national_code = models.CharField(
+        max_length=20, verbose_name=_('National code'), null=True, blank=True)
+    birthdate = models.DateTimeField(
+        verbose_name=_('Birthdate'), null=True, blank=True)
+
+    address = models.TextField(
+        verbose_name=_('Address'), null=True, blank=True)
+    gender = models.CharField(
+        max_length=6, choices=Gender.choices, default=Gender.NONE, verbose_name=_('Gender'))
+    get_newsletter = models.BooleanField(
+        verbose_name=_('Get the newsletter'), null=True, blank=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Created at'))
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name=_('Updated at'))
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
