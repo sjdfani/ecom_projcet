25e17320079a49ef40d509dd20ff332f3d510f19
sajjad <faniam321@gmail.com>
2023-02-10 15:29:27 +0330

    add new model

--- communication/models.py
+++ communication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import CustomUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, verbose_name=_('User'), related_name='message_user'
+    )
+    from_user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, verbose_name=_('From user'), related_name='message_from_user'
+    )
+    message = models.TextField(verbose_name=_('Message'))
+    created_at = models.DateTimeField(auto_now_add=True)
