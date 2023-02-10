dcc669698e788ea9e171781529cf141edd7c7048
sajjad <faniam321@gmail.com>
2023-02-10 12:22:54 +0330

    refactor code

--- users/views.py
+++ users/views.py
@@ -1,31 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import CreateAPIView
 from .serializer import (
     RegisterUserSerializer, RegisterAdminUserSerializer
 )
 from .permissions import IsSuperuser
+from .models import CustomUser
 
 
-class RegisterUser(APIView):
-    def post(self, request):
-        serializer = RegisterUserSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            message = {'create-user': 'create is successful'}
-            return Response(message, status=status.HTTP_201_CREATED)
+class RegisterUser(CreateAPIView):
+    serializer_class = RegisterUserSerializer
+    queryset = CustomUser.objects.all()
 
 
-class RegisterAdminUser(APIView):
+class RegisterAdminUser(CreateAPIView):
     permission_classes = [IsSuperuser]
-
-    def post(self, request):
-        serializer = RegisterAdminUserSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            message = {'create-admin': 'create is successful'}
-            return Response(message, status=status.HTTP_201_CREATED)
+    serializer_class = RegisterAdminUserSerializer
+    queryset = CustomUser.objects.all()
