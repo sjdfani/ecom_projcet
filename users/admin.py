13890e42193be2924c9883a5e778c1a199b08de3
sajjad <faniam321@gmail.com>
2023-02-10 14:35:50 +0330

    complete admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'fullname', 'gender']
+    search_fields = ['email', 'fullname']
+    ordering = ['fullname']
+    fieldsets = (
+        (_('Personal information'), {
+            'fields': ('email', 'fullname', 'national_code', 'home', 'phone', 'birthdate', 'gender', 'address', 'get_newsletter')
+        }),
+        (_('Date information'), {
+            'fields': ('date_joined', 'last_login')
+        }),
+        (_('Other information'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')
+        })
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'fullname', 'national_code', 'home', 'phone', 'birthdate', 'gender', 'address', 'get_newsletter'
+            )
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
