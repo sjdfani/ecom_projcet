a11057126054dc8ccadfec3099733781ce91c175
sajjad <faniam321@gmail.com>
2023-02-11 14:42:43 +0330

    complete admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Favorite, Coupon, Category, Warranty, Product
 
-# Register your models here.
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'created_at')
+    search_fields = ('user', 'product')
+    list_filter = ('user',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('created_by', 'discount', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('discount',)
+
+
+class WarrantyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('user', 'warranty', 'category', 'status', 'created_at')
+    search_fields = ('title', 'description', 'material', 'price')
+    list_filter = ('user', 'status', 'warranty', 'category',)
+
+
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Coupon, CouponAdmin)
+admin.site.register(Warranty, WarrantyAdmin)
+admin.site.register(Product, ProductAdmin)
